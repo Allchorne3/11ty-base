b1ef80f596544941d6ca1a3fe06cd43d7a473e01
Allchorne3 <38890461+Allchorne3@users.noreply.github.com>
2022-06-03 18:17:08 +0100

    Added laravel-mix and page system
    Laravel Mix for asset management and blog page file structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,24 @@
+const { DateTime } = require('luxon');
+var handlebars = require('handlebars');
+handlebars.registerHelper('moment', require('helper-moment'))
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter("postdate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  })
+
+  eleventyConfig.addHandlebarsHelper("reverse", function (arr) {
+    arr.reverse();
+  })
+
+  eleventyConfig.setBrowserSyncConfig({
+    open: "local",
+    hot: true
+  })
+
+
+
   return {
     dir: {
       input: "src", 
