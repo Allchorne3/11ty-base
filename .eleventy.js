6d2be91060f973e25e1189193d7c9abf7a218fa8
Allchorne3 <38890461+Allchorne3@users.noreply.github.com>
2022-06-04 00:39:30 +0100

    edited eleventy config. removed unnecessary code

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,7 @@
-const { DateTime } = require('luxon');
-var handlebars = require('handlebars');
+const handlebars = require('handlebars');
 handlebars.registerHelper('moment', require('helper-moment'))
 
 module.exports = function(eleventyConfig) {
-
-  eleventyConfig.addFilter("postdate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-  })
 
   eleventyConfig.addHandlebarsHelper("reverse", function (arr) {
     arr.reverse();
